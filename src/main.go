de4b93f39d21de5d188e46e76f52ca564b84aec3
TomParsons <tparsons@nd.edu>
2017-10-30 19:53:33 -0400

    Added the iris dataset as a test example. Implemented a toy parser for it in main

--- src/main.go
+++ src/main.go
@@ -1,8 +1,60 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"log"
+	"encoding/csv"
+	"bufio"
+	"io"
+	"strconv"
+	"path/filepath"
+)
+
+
+type IrisData struct{
+	sepalLen float64
+	sepalWid float64
+	petalLen float64
+	petalWid float64
+	class string
+}
 
 func main() {
 	// Testing comments
-	fmt.Printf("hello, world\n")
+	absPath, _ := filepath.Abs("./datasets/iris.data")
+	irisFile, err := os.Open(absPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	iris_reader := csv.NewReader(bufio.NewReader(irisFile))
+
+	var iris []IrisData
+
+	for {
+		line, err := iris_reader.Read()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			log.Fatal(err)
+		}
+
+		sL, _ := strconv.ParseFloat(line[0], 64)
+		sW, _ := strconv.ParseFloat(line[1], 64)
+		pL, _ := strconv.ParseFloat(line[2], 64)
+		pW, _ := strconv.ParseFloat(line[3], 64)
+
+		iris = append(iris, IrisData{
+			sepalLen: sL,
+			sepalWid: sW,
+			petalLen: pL,
+			petalWid: pW,
+			class:    line[4],
+		}, )
+	}
+
+	fmt.Printf("I just read in %d data points\n", len(iris))
+	fmt.Println("The first record is:")
+	fmt.Printf("%+v\n", iris[0])
 }
+
